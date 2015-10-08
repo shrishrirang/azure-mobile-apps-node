523f3c24e5c45be758fa3d2fbe80191aabe20f9f
Dale Anderson <dalander@microsoft.com>
2015-10-07 17:48:40 -0700

    Added middleware tests

--- src/express/tables/middlewareFactory.js
+++ src/express/tables/middlewareFactory.js
@@ -10,18 +10,18 @@ module.exports = function (configuration, router, executeOperation) {
         idRoute = '/:id';
 
     // add operation specific middleware configured by the user
-    configureOperation('read', 'get', [parseQuery(configuration)], [], [defaultRoute, idRoute]);
-    configureOperation('insert', 'post', [parseItem(configuration)], [], [defaultRoute]);
-    configureOperation('insert', 'post', [parseQuery(configuration)], [], [idRoute]); // post with an ID is an undelete operation
-    configureOperation('update', 'patch', [parseItem(configuration)], [], [defaultRoute, idRoute]);
-    configureOperation('delete', 'delete', [parseQuery(configuration)], [], [defaultRoute, idRoute]);
+    configureOperation('read', 'get', [parseQuery(configuration)], [defaultRoute, idRoute]);
+    configureOperation('insert', 'post', [parseItem(configuration)], [defaultRoute]);
+    configureOperation('insert', 'post', [parseQuery(configuration)], [idRoute]); // post with an ID is an undelete operation
+    configureOperation('update', 'patch', [parseItem(configuration)], [defaultRoute, idRoute]);
+    configureOperation('delete', 'delete', [parseQuery(configuration)], [defaultRoute, idRoute]);
 
     // return table specific middleware configured by the user - if no execute middleware has been configured, just return the router we configured
     return !configuration.middleware.execute || configuration.middleware.execute.length === 0
         ? [router]
         : configuration.middleware.execute;
 
-    function configureOperation(operation, verb, pre, post, routes) {
+    function configureOperation(operation, verb, pre, routes) {
         var operationMiddleware = configuration.middleware[verb] || [],
             // if no middleware has been configured, just use the executeOperation middleware
             middleware = operationMiddleware.length === 0
@@ -32,7 +32,6 @@ module.exports = function (configuration, router, executeOperation) {
         if (configuration.authorise || (configuration[operation] && configuration[operation].authorise)) middleware.unshift(authorise);
 
         if (pre) middleware.unshift.apply(middleware, pre);
-        if (post) middleware.push.apply(middleware, post);
 
         routes.forEach(function (route) {
             router[verb](route, middleware);
--- test/express/integration/middleware.tests.js
+++ test/express/integration/middleware.tests.js
@@ -7,14 +7,30 @@ var expect = require('chai').expect,
     mobileApp = require('../../..')();
 
 describe('azure-mobile-apps.express.integration.middleware', function () {
-    it('middleware is mounted in the correct order', function () {
+    it('read middleware is mounted in the correct order', function () {
+        test('read');
+    });
+
+    it('insert middleware is mounted in the correct order', function () {
+        test('insert');
+    });
+
+    it('update middleware is mounted in the correct order', function () {
+        test('update');
+    });
+
+    it('delete middleware is mounted in the correct order', function () {
+        test('delete');
+    });
+
+    function test(operation) {
         var results = [];
 
         mobileApp.use(appendResult(1));
 
         var table = mobileApp.table();
         table.use(appendResult(2), table.execute, appendResult(5));
-        table.read.use(appendResult(3), table.operation, appendResult(4));
+        table[operation].use(appendResult(3), table.operation, appendResult(4));
         mobileApp.tables.add('test', table);
         app.use(mobileApp);
 
@@ -31,5 +47,5 @@ describe('azure-mobile-apps.express.integration.middleware', function () {
                 next();
             }
         }
-    });
+    }
 });
