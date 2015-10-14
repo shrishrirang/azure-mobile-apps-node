5461eadd7ed66e42a73cef58d9133935486f3d09
Dale Anderson <danderson00@gmail.com>
2015-10-13 21:56:54 -0700

    Added comment

--- src/express/tables/index.js
+++ src/express/tables/index.js
@@ -32,6 +32,7 @@ module.exports = function (configuration) {
     router.add = function (name, definition) {
         assert(name, 'A table name was not specified');
 
+        // if the definition doesn't have a router function, wrap it in a table definition object
         if(!definition || typeof definition.router !== 'function')
             definition = table(definition);
         configuration.tables[name] = definition;
