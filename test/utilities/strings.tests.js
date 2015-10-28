4b28fbf51586bccfc258833cb1cbb93d36706634
Alexander Hramov <alexander.hramov@gmail.com>
2015-10-28 20:36:27 +0300

    Fxi test to extractEtag name

--- test/utilities/strings.tests.js
+++ test/utilities/strings.tests.js
@@ -2,9 +2,9 @@ var expect = require('chai').expect,
     strings = require('../../src/utilities/strings');
 
 describe('azure-mobile-apps.utilities.strings', function () {
-    describe('getVersiomFromIfMatch', function () {
+    describe('extractEtag', function () {
         it('removes starting and finishing double quotes and remove escaping from quotes in the middle', function () {
-            expect(strings.getVersiomFromIfMatch('"test \"inside\" test"')).to.equal('test "inside" test');
+            expect(strings.extractEtag('"test \"inside\" test"')).to.equal('test "inside" test');
         })
     })
 });
