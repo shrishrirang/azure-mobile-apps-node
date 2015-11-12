bd0fd6b83cc0dce07c1b1d8dca82d0c1da82645e
Adrian Hall <adrian@shellmonger.com>
2015-11-12 15:26:18 -0800

    tabs to spaces

--- src/auth/normalizeClaims.js
+++ src/auth/normalizeClaims.js
@@ -3,20 +3,20 @@
 // ----------------------------------------------------------------------------
 
 module.exports = function (claims) {
-	var normalizedClaims = claims.reduce(function (obj, value, idx) {
-		obj[value['provider_name']] = value;
-		return obj;
-	}, {});
+    var normalizedClaims = claims.reduce(function (obj, value, idx) {
+        obj[value['provider_name']] = value;
+        return obj;
+    }, {});
 	
-	for (var idp in normalizedClaims) {
-		normalizedClaims[idp].claims = normalizedClaims[idp].user_claims.reduce(function (obj, value, idx) {
-			obj[value.typ] = value.val;
+    for (var idp in normalizedClaims) {
+        normalizedClaims[idp].claims = normalizedClaims[idp].user_claims.reduce(function (obj, value, idx) {
+            obj[value.typ] = value.val;
 			if (value.typ.startsWith('http://schemas.xmlsoap.org/ws')) {
-				obj[value.typ.slice(value.typ.lastIndexOf('/')+1)] = value.val;
-			}
-			return obj;
-		}, {});
-	}
+			    obj[value.typ.slice(value.typ.lastIndexOf('/')+1)] = value.val;
+            }
+            return obj;
+        }, {});
+    }
 	
-	return normalizedClaims;
+    return normalizedClaims;
 };
