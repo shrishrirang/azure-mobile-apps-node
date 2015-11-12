dae5b143f7eb4546d3313eec855152e17e355ea3
Matt Mason <mamaso@microsoft.com>
2015-11-12 11:46:54 -0800

    Etag utility function naming clarity

--- src/express/middleware/eTag.js
+++ src/express/middleware/eTag.js
@@ -2,19 +2,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 var queries = require('../../query'),
-    packEtag = require('../../utilities/strings').packEtag;
+    getEtagFromVersion = require('../../utilities/strings').getEtagFromVersion;
 
 module.exports = {
     singleResult: function (req, res, next) {
         if (res.results && res.results.version) {
-            res.set('ETag', packEtag(res.results.version));
+            res.set('ETag', getEtagFromVersion(res.results.version));
         }
         next();
     },
 
     readIdResult: function (req, res, next) {
         if (res.results && res.results.length === 1 && res.results[0].version) {
-            res.set('ETag', packEtag(res.results[0].version));
+            res.set('ETag', getEtagFromVersion(res.results[0].version));
         }
         next();
     }
