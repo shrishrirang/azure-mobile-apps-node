23751681d9c73ae731caa7e98c4654770184754f
Adrian Hall <adrian@shellmonger.com>
2015-11-13 11:56:15 -0800

    Fixed tabs to spaces

--- src/auth/normalizeClaims.js
+++ src/auth/normalizeClaims.js
@@ -7,16 +7,16 @@ module.exports = function (claims) {
         object[value['provider_name']] = value;
         return object;
     }, {});
-	
+
     for (var idp in normalizedClaims) {
         normalizedClaims[idp].claims = normalizedClaims[idp].user_claims.reduce(function (object, value, index) {
             object[value.typ] = value.val;
-			if (value.typ.indexOf('http://schemas.xmlsoap.org/ws') !== -1) {
-			    object[value.typ.slice(value.typ.lastIndexOf('/')+1)] = value.val;
+            if (value.typ.indexOf('http://schemas.xmlsoap.org/ws') !== -1) {
+                object[value.typ.slice(value.typ.lastIndexOf('/') + 1)] = value.val;
             }
             return object;
         }, {});
     }
-	
+
     return normalizedClaims;
 };
