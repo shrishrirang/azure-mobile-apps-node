ee55e2f55cfd449255ec933ead90c44a60f95c92
Adrian Hall <adrianha@microsoft.com>
2015-11-20 15:00:52 -0800

    Added push-on-insert sample

---
+++ samples/push-on-insert/tables/TodoItem.js
@@ -0,0 +1,41 @@
+var azureMobileApps = require('azure-mobile-apps'),
+    promises = require('azure-mobile-apps/src/utilities/promises');
+
+var table = azureMobileApps.table();
+
+// Defines the list of columns
+table.columns = {
+	"userId": "string",
+	"text": "string",
+	"complete": "boolean"
+};
+// Turns off dynamic schema
+table.dynamicSchema = false;
+
+// Must be authenticated for this to work
+table.access = 'authenticated';
+
+// When adding record, send a push notification
+table.insert(function (context) {
+    // For details of the Notification Hubs JavaScript SDK, 
+    // see https://azure.microsoft.com/en-us/documentation/articles/notification-hubs-nodejs-how-to-use-notification-hubs/
+    
+    // The tag is used to send the push notification to a subset of users
+    var tag = '#sometag';
+    
+    // This push uses a template mechanism, so we need a template.
+    var payload = '<?xml version="1.0" encoding="utf-8"?><wp:Notification xmlns:wp="WPNotification"><wp:Toast><wp:Text1>A new issue has arrived</wp:Text1></wp:Toast></wp:Notification>';
+    
+	// Create the push notification - we are using the send function 
+    // The Notification Hubs JavaScript SDK uses callbacks, so we have
+    // to wrap the call in a promise.
+    promises.wrap(context.push.mpns.send)(tag, payload, 'toast', 22)
+    .then(function (err) {
+        if (err) {
+            console.error("Error while sending push notification: " + err);
+        }
+        context.execute();
+    });
+});
+
+module.exports = table;
