0d5dbf30daf03e44f3b9219743b2a96a402f1acd
Dale Anderson <dalander@microsoft.com>
2015-12-11 17:12:25 -0800

    Turned off dynamic schema for round trip tables

--- e2etest/server.js
+++ e2etest/server.js
@@ -22,7 +22,7 @@ mobileApp.tables.add('blog_comments', { columns: { postId: 'string', commentText
 mobileApp.tables.add('blog_posts', { columns: { title: 'string', commentCount: 'number', showComments: 'boolean', data: 'string' } });
 mobileApp.tables.add('dates', { columns: { date: 'date', dateOffset: 'date' } });
 mobileApp.tables.add('movies', { columns: { title: 'string', duration: 'number', mpaaRating: 'string', releaseDate: 'date', bestPictureWinner: 'boolean' } });
-mobileApp.tables.add('IntIdRoundTripTable', { autoIncrement: true, columns: { name: 'string', date1: 'date', bool: 'boolean', integer: 'number', number: 'number' } });
+mobileApp.tables.add('IntIdRoundTripTable', { autoIncrement: true, columns: { name: 'string', date1: 'date', bool: 'boolean', integer: 'number', number: 'number' }, dynamicSchema: false });
 mobileApp.tables.add('intIdMovies', { autoIncrement: true, columns: { title: 'string', duration: 'number', mpaaRating: 'string', releaseDate: 'date', bestPictureWinner: 'boolean' } });
 mobileApp.tables.add('OfflineReady');
 mobileApp.tables.add('OfflineReadyNoVersionAuthenticated', { authorize: true });
