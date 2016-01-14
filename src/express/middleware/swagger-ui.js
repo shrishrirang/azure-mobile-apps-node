e744faac9fd4c778cb722ab9654bb0526578dc65
Dale Anderson <danderson00@gmail.com>
2016-01-14 14:14:06 -0800

    swagger ui https fix

--- src/express/middleware/swagger-ui.js
+++ src/express/middleware/swagger-ui.js
@@ -21,8 +21,10 @@ module.exports = function(configuration) {
 
         function swaggerUrl() {
             // this is a bit of a hack and assumes swagger metadata is exposed in the parent directory
-            var swaggerPath = path.join(req.originalUrl, '..').replace(/\\/g, '/');
-            return req.protocol + '://' + req.get('host') + swaggerPath;
+            var swaggerPath = path.join(req.originalUrl, '..').replace(/\\/g, '/'),
+                // requests are always http from the Azure Web Apps front end to the worker - detect using headers that ARR attaches
+                protocol = (req.headers['x-forwarded-proto'] === 'https' || req.headers['x-arr-ssl']) ? 'https' : 'http';
+            return protocol + '://' + req.get('host') + swaggerPath;
         }
     };
 
