0bbfe5b12684f5b6dc18fe1e02a0a6ba636f2d51
Dale Anderson <dalander@microsoft.com>
2016-04-05 09:45:36 -0700

    Fixed statement tests

--- test/data/sqlite/statements/createTable.tests.js
+++ test/data/sqlite/statements/createTable.tests.js
@@ -4,42 +4,42 @@
 // there should be more tests here, but SQL tests can be fragile
 // full coverage should be provided by integration tests
 
-var expect = require('chai').expect,
-    mssql = require('mssql'),
-    statements = require('../../../../src/data/sqlite/statements');
+var expect = require("chai").expect,
+    mssql = require("mssql"),
+    statements = require("../../../../src/data/sqlite/statements");
 
-describe('azure-mobile-apps.data.sqlite.statements', function () {
-    describe('createTable', function () {
+describe("azure-mobile-apps.data.sqlite.statements", function () {
+    describe("createTable", function () {
         var createTable = statements.createTable;
 
-        it('generates create statement with string id', function () {
-            var statement = createTable({ name: 'table' }, { id: '1', text: 'test' });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)')
+        it("generates create statement with string id", function () {
+            var statement = createTable({ name: "table" }, { id: "1", text: "test" });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)")
         });
 
-        it('generates create statement with numeric id', function () {
-            var statement = createTable({ name: 'table' }, { id: 1, text: 'test' });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] INTEGER PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)')
+        it("generates create statement with numeric id", function () {
+            var statement = createTable({ name: "table" }, { id: 1, text: "test" });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] INTEGER PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)")
         });
 
-        it('generates create statement with string id if none is provided', function () {
-            var statement = createTable({ name: 'table' }, { text: 'test' });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)')
+        it("generates create statement with string id if none is provided", function () {
+            var statement = createTable({ name: "table" }, { text: "test" });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)")
         });
 
-        it('generates integer identity column when autoIncrement is specified', function () {
-            var statement = createTable({ name: 'table', autoIncrement: true }, { text: 'test' });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] INTEGER PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)')
+        it("generates integer identity column when autoIncrement is specified", function () {
+            var statement = createTable({ name: "table", autoIncrement: true }, { text: "test" });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] INTEGER PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL)")
         });
 
-        it('generates create statement with predefined columns', function () {
-            var statement = createTable({ name: 'table', columns: { number: 'number' } }, { text: 'test' });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL,[number] REAL)')
+        it("generates create statement with predefined columns", function () {
+            var statement = createTable({ name: "table", columns: { number: "number" } }, { text: "test" });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[text] TEXT NULL,[number] REAL)")
         });
 
-        it('generates create statement without an item', function () {
-            var statement = createTable({ name: 'table', columns: { number: 'number' } });
-            expect(statement.sql).to.equal('CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0,[number] REAL)')
+        it("generates create statement without an item", function () {
+            var statement = createTable({ name: "table", columns: { number: "number" } });
+            expect(statement.sql).to.equal("CREATE TABLE [table] ([id] TEXT PRIMARY KEY,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),deleted INTEGER NOT NULL DEFAULT 0,[number] REAL)")
         });
     });
 });
--- test/data/sqlite/statements/updateSchema.tests.js
+++ test/data/sqlite/statements/updateSchema.tests.js
@@ -4,44 +4,53 @@
 // there should be more tests here, but SQL tests can be fragile
 // full coverage should be provided by integration tests
 
-var expect = require('chai').expect,
-    mssql = require('mssql'),
-    statements = require('../../../../src/data/sqlite/statements');
+var expect = require("chai").use(require("chai-subset")).expect,
+    mssql = require("mssql"),
+    statements = require("../../../../src/data/sqlite/statements");
 
-describe('azure-mobile-apps.data.sqlite.statements', function () {
-    describe('updateSchema', function () {
+describe("azure-mobile-apps.data.sqlite.statements", function () {
+    describe("updateSchema", function () {
         var updateSchema = statements.updateSchema;
 
-        it('generates simple statement', function () {
-            var statement = updateSchema({ name: 'table' }, [{ name: 'id' }, { name: 'version' }, { name: 'createdAt' }, { name: 'updatedAt' }, { name: 'deleted' }], { id: 1, text: 'test' });
-            expect(statement.sql).to.equal('ALTER TABLE [table] ADD [text] TEXT NULL');
+        it("generates simple statement", function () {
+            var statement = updateSchema({ name: "table" }, [{ name: "id" }, { name: "version" }, { name: "createdAt" }, { name: "updatedAt" }, { name: "deleted" }], { id: 1, text: "test" });
+            expect(statement).to.containSubset([{ sql: "ALTER TABLE [table] ADD COLUMN [text] TEXT NULL" }]);
         });
 
-        it('generates system properties if missing', function () {
-            var statement = updateSchema({ name: 'table' }, [{ name: 'id' }], { id: 1, text: 'test' });
-            expect(statement.sql).to.equal('ALTER TABLE [table] ADD [text] TEXT NULL,version TEXT NOT NULL DEFAULT 1,createdAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,deleted INTEGER NOT NULL DEFAULT 0');
+        it("generates system properties if missing", function () {
+            var statement = updateSchema({ name: "table" }, [{ name: "id" }], { id: 1, text: "test" });
+            expect(statement).to.containSubset([
+                { sql: "ALTER TABLE [table] ADD COLUMN [text] TEXT NULL" },
+                { sql: "ALTER TABLE [table] ADD COLUMN version TEXT NOT NULL DEFAULT 1" },
+                { sql: "ALTER TABLE [table] ADD COLUMN createdAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))" },
+                { sql: "ALTER TABLE [table] ADD COLUMN updatedAt TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))" },
+                { sql: "ALTER TABLE [table] ADD COLUMN deleted INTEGER NOT NULL DEFAULT 0" }
+            ]);
         });
 
-        it('correctly handles missing system properties that exist in item', function () {
-            var statement = updateSchema({ name: 'table' }, [{ name: 'id' }, { name: 'createdAt' }, { name: 'updatedAt' }, { name: 'deleted' }], { id: 1, text: 'test', version: 'someVersion' });
-            expect(statement.sql).to.equal('ALTER TABLE [table] ADD [text] TEXT NULL,version TEXT NOT NULL DEFAULT 1');
+        it("correctly handles missing system properties that exist in item", function () {
+            var statement = updateSchema({ name: "table" }, [{ name: "id" }, { name: "createdAt" }, { name: "updatedAt" }, { name: "deleted" }], { id: 1, text: "test", version: "someVersion" });
+            expect(statement).to.containSubset([
+                { sql: "ALTER TABLE [table] ADD COLUMN [text] TEXT NULL" },
+                { sql: "ALTER TABLE [table] ADD COLUMN version TEXT NOT NULL DEFAULT 1" }
+            ]);
         });
 
-        it('generates statement for predefined columns', function () {
+        it("generates statement for predefined columns", function () {
             var statement = updateSchema(
-                { name: 'table', columns: { 'text': 'string' } },
-                [{ name: 'id' }, { name: 'createdAt' }, { name: 'updatedAt' }, { name: 'deleted' }, { name : 'version' }],
+                { name: "table", columns: { "text": "string" } },
+                [{ name: "id" }, { name: "createdAt" }, { name: "updatedAt" }, { name: "deleted" }, { name : "version" }],
                 { id: 1 }
             );
-            expect(statement.sql).to.equal('ALTER TABLE [table] ADD [text] TEXT NULL');
+            expect(statement).to.containSubset([{ sql: "ALTER TABLE [table] ADD COLUMN [text] TEXT NULL" }]);
         });
 
-        it('generates statement for predefined columns when item is not supplied', function () {
+        it("generates statement for predefined columns when item is not supplied", function () {
             var statement = updateSchema(
-                { name: 'table', columns: { 'text': 'string' } },
-                [{ name: 'id' }, { name: 'createdAt' }, { name: 'updatedAt' }, { name: 'deleted' }, { name : 'version' }]
+                { name: "table", columns: { "text": "string" } },
+                [{ name: "id" }, { name: "createdAt" }, { name: "updatedAt" }, { name: "deleted" }, { name : "version" }]
             );
-            expect(statement.sql).to.equal('ALTER TABLE [table] ADD [text] TEXT NULL');
+            expect(statement).to.containSubset([{ sql: "ALTER TABLE [table] ADD COLUMN [text] TEXT NULL" }]);
         });
     });
 });
