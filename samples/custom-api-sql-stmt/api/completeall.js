528596aa1eeb25eb2b2f49ff8c6125a8c820ce9a
Dale Anderson <dalander@microsoft.com>
2016-04-11 11:58:17 -0700

    Added example of executing a stored procedure

--- samples/custom-api-sql-stmt/api/completeall.js
+++ samples/custom-api-sql-stmt/api/completeall.js
@@ -1,9 +1,24 @@
 var api = {
+    // an example of executing a SQL statement directly
     get: (request, response, next) => {
         var query = {
-            sql: 'UPDATE TodoItem SET complete=@completed',
+            sql: 'UPDATE TodoItem SET complete = @completed',
             parameters: [
-                { name: 'completed', value: request.params.completed }
+                { name: 'completed', value: request.query.completed }
+            ]
+        };
+
+        request.azureMobile.data.execute(query)
+        .then(function (results) {
+            response.json(results);
+        });
+    },
+    // an example of executing a stored procedure
+    post: (request, response, next) => {
+        var query = {
+            sql: 'EXEC completeAllStoredProcedure @completed',
+            parameters: [
+                { name: 'completed', value: request.query.completed }
             ]
         };
 
