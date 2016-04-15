8b003cf5445d14258fed4f4c93354c12b779039e
Dale Anderson <dalander@microsoft.com>
2016-04-15 10:07:22 -0700

    Added authentication

--- samples/custom-api-routing/app.js
+++ samples/custom-api-routing/app.js
@@ -4,13 +4,13 @@
 var express = require('express'),
     azureMobileApps = require('azure-mobile-apps'),
 
-    // Obtain our custom API - it exports an express Router    
+    // Obtain our custom API - it exports an express Router
     categoryApi = require('./api/category');
 
 // Create a standard Express app
 var app = express();
 
-// Create a mobile app with default configuration  
+// Create a mobile app with default configuration
 var mobile = azureMobileApps();
 
 // Add a single table with dynamic schema that is disabled through the REST API
@@ -21,7 +21,7 @@ app.use(mobile);
 
 // Register the router we configure in our custom API module
 // This must be done after registering the mobile app
-app.use('/api/items', categoryApi);
+app.use('/api/items', categoryApi(mobile.configuration));
 
 // Listen for requests
 app.listen(process.env.PORT || 3000);
