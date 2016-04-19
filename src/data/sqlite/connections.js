e4a48d3d16c571efb449fc5dd84de830424005d9
Dale Anderson <dalander@microsoft.com>
2016-04-19 11:23:38 -0700

    Added specific error for missing sqlite3 module

--- src/data/sqlite/connections.js
+++ src/data/sqlite/connections.js
@@ -1,13 +1,16 @@
-var sqlite3 = require('sqlite3'),
-    connections = {};
-    
+var sqlite3, connections = {};
+
+try {
+    sqlite3 = require('sqlite3')
+} catch(ex) {
+    throw new Error('To use the sqlite data provider, you must install the sqlite3 module by running "npm i sqlite3"');
+}
+
 module.exports = function (configuration) {
     var filename = (configuration && configuration.filename) || ':memory:';
-    
+
     if(!connections[filename])
         connections[filename] = new sqlite3.cached.Database(filename);
-        
+
     return connections[filename];
 };
-
-
