52ff7840e23f699e5a601dfde42526650c0016ea
danderson00 <danderson00@gmail.com>
2016-04-22 15:25:33 -0700

    Fix dynamic schema bug with databaseTableName

--- src/data/mssql/statements/getColumns.js
+++ src/data/mssql/statements/getColumns.js
@@ -5,7 +5,7 @@ module.exports = function (tableConfig) {
     return {
         sql: "SELECT COLUMN_NAME AS name, DATA_TYPE AS type FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table AND TABLE_SCHEMA = @schema",
         parameters: [
-            { name: 'table', value: tableConfig.name },
+            { name: 'table', value: tableConfig.containerName || tableConfig.databaseTableName || tableConfig.name },
             { name: 'schema', value: tableConfig.schema || 'dbo' }
         ]
     }
