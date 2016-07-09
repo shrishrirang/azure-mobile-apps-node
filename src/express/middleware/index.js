cb0b5e24ec4c360e5637dc792ff92758042f3bc9
Dale Anderson <Dale Anderson>
2016-07-08 18:52:02 -0700

    Added copyright

--- src/express/middleware/index.js
+++ src/express/middleware/index.js
@@ -1,3 +1,6 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
 module.exports = function (configuration) {
     return function (name) {
         return require('./' + name)(configuration);
